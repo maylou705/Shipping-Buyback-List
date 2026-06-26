44f72dec073032ee877cfaf226bba9b820e237ea
maylou705 <maylou705@gmail.com>
2026-06-26 18:16:26 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'LIQOA Ship',
+  title: 'Shipping Buyback List',
   description: '発送管理システム',
 }
 
